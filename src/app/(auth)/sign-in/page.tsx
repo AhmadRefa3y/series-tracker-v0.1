8922fe2979edc9e585bb25bf16a3853ac4aa0826
Ahmed omar <ahmedrefay3000@gmail.com>
2026-03-29 22:34:44 +0200

    Fix prisma errors and sign in bg img

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -24,15 +24,9 @@ const SignIn = async (props: {
     return redirect(callbackUrl || "/");
   }
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-12 bg-[#f5f5f7] text-foreground relative">
-      <div className="absolute inset-0 z-0">
-        <Image
-          src="https://trakt.tv/assets/auth/breaking-bad-blur-756cc8baa3074a86282cb2672af547c20a1e529b191542c4c02a72c121209486.jpg.webp"
-          alt="Dramatic landscape with silhouettes"
-          fill
-          className="bg-transparent"
-        />
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-12 bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 text-foreground relative overflow-hidden">
+      <div className="absolute inset-0 z-0 bg-[url('https://images.unsplash.com/photo-1536440136628-849c177e76a1?q=80&w=2525&auto=format&fit=crop')] bg-cover bg-center opacity-30"></div>
+      <div className="absolute inset-0 z-0 bg-gradient-to-t from-black/60 via-transparent to-black/40"></div>
       <div className="w-full max-w-xs pt-10  space-y-8  rounded-lg shadow-xl bg-card z-10 relative">
         <div className="text-center">
           <Link
